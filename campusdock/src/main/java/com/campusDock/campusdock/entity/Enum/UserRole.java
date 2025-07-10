e96ca70f216c197834f32fc340ac03762eb43aba
Sayed Haneef <sayedhaneef31@gmail.com>
2025-07-10 13:59:58 +0530

    Fixed role issue

--- campusdock/src/main/java/com/campusDock/campusdock/entity/Enum/UserRole.java
+++ campusdock/src/main/java/com/campusDock/campusdock/entity/Enum/UserRole.java
@@ -3,6 +3,7 @@ package com.campusDock.campusdock.entity.Enum;
 public enum UserRole  {
     STUDENT,
     FACULTY,
+    STAFF,
     CANTEEN_OWNER,
     ADMIN,
     SUPER_ADMIN
--- campusdock/src/main/java/com/campusDock/campusdock/service/ServiceImpl/UserServiceImpl.java
+++ campusdock/src/main/java/com/campusDock/campusdock/service/ServiceImpl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.campusDock.campusdock.service.ServiceImpl;
 import com.campusDock.campusdock.entity.College;
 import com.campusDock.campusdock.dto.CreateUserDto;
 //import com.campusDock.campusdock.entity.DTO.UserResponseDto;
+import com.campusDock.campusdock.entity.Enum.UserRole;
 import com.campusDock.campusdock.entity.User;
 import com.campusDock.campusdock.repository.CollegeRepo;
 import com.campusDock.campusdock.repository.UserRepo;
@@ -30,10 +31,9 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User createUser(CreateUserDto createUserDto) {
+
         // Extract domain (e.g. abes.ac.in) from email
         String domain = createUserDto.getEmail().substring(createUserDto.getEmail().indexOf("@") + 1);
-
-
 
         College college = collegeRepo.findByDomain(domain)
                 .orElseThrow(() -> new RuntimeException("College not registered for domain: " + domain));
@@ -46,10 +46,25 @@ public class UserServiceImpl implements UserService {
         user.setPassword(createUserDto.getPassword());
         user.setEmail(createUserDto.getEmail());
         user.setCollege(college);
+        user.setRole(detectUserRoleFromEmail(createUserDto.getEmail()));
 
         return userRepo.save(user);
 
 
+    }
+
+    public UserRole detectUserRoleFromEmail(String email) {
+        String localPart = email.substring(0, email.indexOf("@"));        // sayed.22b0121094
+        String identifier = localPart.contains(".") ?
+                localPart.split("\\.")[1] : "";
+
+        if (identifier.matches(".*\\d.*")) {
+            return UserRole.STUDENT; // contains numbers → student roll pattern
+        } else if (localPart.contains("admin") || localPart.contains("staff") || localPart.contains("hr")) {
+            return UserRole.STAFF;
+        } else {
+            return UserRole.FACULTY; // default to faculty if it’s non-numeric
+        }
     }
 
 
